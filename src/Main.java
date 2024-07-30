118914206c9273380636e770456c8d38cd77deae
Zakhar <info@zspirin.ru>
2024-07-30 20:07:35 +0300

    test

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+       ServerWindow serverWindow = new ServerWindow();
+        new ClientGUI(serverWindow);
+        new ClientGUI(serverWindow);
+    }
+}
